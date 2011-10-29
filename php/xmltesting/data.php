bba604952c958cb0445e698e8466e2d7b18d30b9
rezabarus <rezabarus@barus-stuff.com>
2011-10-30 05:47:56 +0700

    Upload ke database

---
+++ php/xmltesting/data.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Test Input Data</title>
+	</head>
+		<body>
+			<form method="POST" action="input_data.php">
+				<table border="1">
+					<tr><td>Name</td><td><input type="text" name="nama"></td></tr>
+					<tr><td>Description</td><td><input type="text" name="description"></td></tr>
+					<tr><td><input type="submit" value="submit"></td><td><input type="reset" value="reset"></td></tr>
+				</table>
+			</form>
+		</body>
